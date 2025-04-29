ad3f7c31c8abbcf9a7bdb78d715912ae779751d6
yeoro <dufgh1009@gmail.com>
2025-04-29 18:23:38 +0900

    Initial commit

---
+++ src/main/java/com/yeoro/springoauthauthorizationserver/SpringOauthAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeoro.springoauthauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthAuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeoro/springoauthauthorizationserver/SpringOauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoro.springoauthauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
